ea1871e839feb2e1cd34bc864e1d8ec4b8abed0a
bayzinbarkin <barkin.bayzin@bilgeadamboost.com>
2022-08-31 12:53:48 +0300

    Add project files.

---
+++ ENTITIES/Entity/Concrete/Article.cs
@@ -0,0 +1,33 @@
+﻿using ENTITIES.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.Entity.Concrete
+{
+    [Table("Articles")]
+    public class Article:BaseEntity
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string ArticlePicture { get; set; }
+
+        [Required]
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public User Author { get; set; }
+
+    }
+}
---
+++ ENTITIES/Entity/Concrete/User.cs
@@ -0,0 +1,20 @@
+﻿using ENTITIES.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.Entity.Concrete
+{
+    [Table("Users")]
+    public class User :BaseEntity
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
